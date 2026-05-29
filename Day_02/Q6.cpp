b8b5064f1dcc7f58e351e727cfdee60bb81a1934
arussh09 <baruaarush99@gmail.com>
2026-05-29 12:46:58 +0530

    Question 6

---
+++ Day_02/Q6.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void rev(int n){
+    int num=n;
+    int og=n;
+    int cnt=0;
+    while (n!=0)
+    {
+        n = n/10;
+        cnt++;
+    }
+    int rem =0;
+    int revs =0;
+    while(cnt!=0){
+        rem = num%10;
+        revs  = revs*10 + rem;
+        num = num/10;
+        cnt--;
+    }
+    cout<<"Reverse Of "<<og<<" : "<<revs;
+}
+int main(){
+    rev(12345);
+}
