318751c5c6828cc16398cf88851cdfc8a7b65307
arussh09 <baruaarush99@gmail.com>
2026-05-31 20:50:07 +0530

    Question 13

---
+++ Day_04/Q13.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void fibonacci(int n){
+    int a =0;
+    int b =1;
+    int c;
+    for (int i = 1; i <=n; i++)
+    {
+        cout<<a<<endl;
+        c=a+b;
+        a =b;
+        b=c;
+        
+    }
+    
+}
+int main(){
+    fibonacci(10);
+}
