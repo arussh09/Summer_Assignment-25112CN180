c301732c956e91d90a6678f0ddbb5ac73be7c887
arussh09 <baruaarush99@gmail.com>
2026-06-14 16:42:16 +0530

    Day_12

---
+++ Day_12/Q46.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool isArmstrong(int n) {
+    int original = n;
+    int sum = 0;
+
+    while (n != 0) {
+        int digit = n % 10;
+        sum += digit * digit * digit;
+        n /= 10;
+    }
+
+    return original == sum;
+}
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isArmstrong(num))
+        cout << num << " is an Armstrong Number";
+    else
+        cout << num << " is not an Armstrong Number";
+
+    return 0;
+}
